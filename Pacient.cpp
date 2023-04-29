b9e3e366e937363d5e83cc7c68d656cea1737c6d
Martinazoo <paumartinnadal@gmail.com>
2023-04-29 17:50:32 +0200

    Sintaxis del Pacient cpp arreglada

--- Pacient.cpp
+++ Pacient.cpp
@@ -6,14 +6,14 @@
 		
 		/* Pre: cert */
 		/* Post: crea un pacient buit */		
-		Pacient()
+		Pacient::Pacient()
         {
 
         }
 
         /* Pre: cert */
 		/* Post: crea un pacient de nom 'nom' */	
-		Pacient(string nom)
+		Pacient::Pacient(string nom)
         {
             this->nom = nom;
         }
@@ -21,7 +21,7 @@
 		/* Pre: cert */
 		/* Post: crea un pacient amb els valors de 'nom',
            'edat', 'motiu' i 'gravetat' rebuts per paràmetre */
-		Pacient(string nom, int edat, string motiu, int gravetat)
+		Pacient::Pacient(string nom, int edat, string motiu, int gravetat)
         {
             this-> nom = nom;
             this-> edat = edat;
@@ -36,7 +36,7 @@
 		/* Pre: cert */
 		/* Post: el valor de gravetat del pacient paràmetre 
 		   implícit	passa a ser el valor de 'gravetat' */
-		void actualitzaEstat(int gravetat)
+		void Pacient::actualitzaEstat(int gravetat)
         {
             this-> gravetat = gravetat;
         }
@@ -47,14 +47,14 @@
 
 		/* Pre: cert */
 		/* Post: retorna el nom del pacient paràmetre implicit */
-		string getNom() const
+		string Pacient::getNom() const
         {
             return nom;
         }
 
 		/* Pre: cert */
 		/* Post: retorna l'edat del pacient paràmetre implicit */
-		int getEdat() const
+		int Pacient::getEdat() const
         {
             return edat;
         }
@@ -62,7 +62,7 @@
 		/* Pre: cert */
 		/* Post: retorna el motiu de l'ingrés del pacient paràmetre 
 		   implicit */
-		string getMotiu() const
+		string Pacient::getMotiu() const
         {
             return motiu;
         }
@@ -70,7 +70,7 @@
 		/* Pre: cert */
 		/* Post: retorna la gravetat de l'estat del pacient paràmetre
 		   implicit */		
-		int getGravetat() const
+		int Pacient::getGravetat() const
         {
             return gravetat;
         }
@@ -79,7 +79,9 @@
 		/* Post: retorna un booleà que indica si l'estat de gravetat
 		   del pacient paràmetre implícit i el del pacient 'p' són
 		   identics (true) o no ho són (false) */
-		bool compare(const Pacient &p) const;
+		bool Pacient::compare(const Pacient &p) const{
+
+		}
 
 
 		// PODEU AFEGIR ELS MÈTODES QUE NECESSITEU
@@ -94,9 +96,13 @@
 		/* Post: el pacient 'p' passa a tenir els valors llegits del canal
 		   estàndard d'entrada com a nom, edat, motiu i gravetat, 
 		   respectivament */			
-		friend istream& operator>>(istream &is, Pacient &p);
+		istream& operator>>(istream &is, Pacient &p){
+
+		}
 		
 		/* Pre: cert */
 		/* Post: s'han escrit els atributs nom, edat, motiu i gravetat del
            pacient 'p' al canal estàndard de sortida */
-		friend ostream& operator<<(ostream &os, const Pacient &p);
+		ostream& operator<<(ostream &os, const Pacient &p){
+			
+		}
