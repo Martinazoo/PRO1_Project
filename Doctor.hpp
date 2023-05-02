bc83821bb4d3bf2090d139782f4d1e04ffbe7f5b
Martinazoo <paumartinnadal@gmail.com>
2023-05-02 12:09:08 +0200

    Classe doctor

---
+++ Doctor.hpp
@@ -0,0 +1,19 @@
+#ifndef DOCTOR_HPP
+#define DOCTOR_HPP
+#include "Visita.hpp"
+#include "Pacient.hpp"
+
+using namespace std; 
+
+/** Representació d'un doctor.
+ *
+ *  Consta de quatre atributs per emmagatzemar el nom (string),
+ *  edat (enter), motiu de l'ingrés (string) i gravetat de 
+ *  l'estat de salut (int) d'un pacient.
+ */
+
+class Doctor {
+
+};
+
+#endif
