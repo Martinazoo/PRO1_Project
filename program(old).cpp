c35f3cf96048dcf744e93a260ddee0a026b6d247
MarcBlancSans <marc.blanc@estudiantat.upc.edu>
2023-05-16 18:00:31 +0200

    a

---
+++ program(old).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Hospital.hpp"
+#include "PriorityQueue.hpp"
+#include "BST.hpp"
+using namespace std;
+
+/** Programa principal
+ *
+ * main() crea l'hospital, fa la lectura d'instruccions i
+ * l'escriptura de resultats. Les operacions estan definides i
+ * implementades en les classes Hospital, Pacient, Doctor, Visita,
+   Data, PriorityQueue i BST
+ */
+
+int main() {
+
+
+}
